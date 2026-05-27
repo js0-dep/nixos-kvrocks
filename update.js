a8b9cc3e0857a701529190b3e86880705b14f663
js0.site <jssite@googlegroups.com>
2026-05-28 06:08:55 +0800

    refactor: use yargs to parse required tag positional argument in update.js

--- update.js
+++ update.js
@@ -2,11 +2,23 @@
 
 import verJson from "@3-/nix/verJson.js";
 import { cd, $ } from "zx";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-const ROOT = import.meta.dirname;
+const ROOT = import.meta.dirname,
+  argv = yargs(hideBin(process.argv))
+    .command("$0 <tag>", "Update nixos-kvrocks", (yargs) => {
+      yargs.positional("tag", {
+        describe: "kvrocks tag to update to",
+        type: "string",
+      });
+    })
+    .demandCommand(1)
+    .parse();
+
 cd(ROOT);
 
-await verJson(ROOT, "apache/kvrocks", process.argv[2]);
+await verJson(ROOT, "apache/kvrocks", argv.tag);
 
 await $`./update_dep.py`;
 
